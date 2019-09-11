5531225e849bed4effb69e7c1045cf637004b5d2
pcutufia <54905594+pcutufia@users.noreply.github.com>
2019-09-11 16:39:22 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (1).js
+++ 5-InstruccionWhile/jsIteraciones (1).js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-	alert('iteración while');
+	var contador = 0;
+
+	while( contador < 10) {
+
+	console.log("Cont:" + contador);
+
+	contador++;
+}	
+console.log("Afuera cont: " + contador);
 
 
+// con -- va decreciendo y ++ incrementando
+// if( contador == 4 { break; }) salis en el contador nº 4
+// continue: deja de hacer esa iteracion y pasa a la siguiente. Evalua la condiciòn.
+// if ( contador % 2) {continue; }  console.log(contador);} 
+
+// while simpre vuelve a iniciar (repetitiva), no como el if que pasa de largo despues de validar.
 }//FIN DE LA FUNCIÓN
